d55c5ec32542d9c3e9ba36fe3e8b38f583cf9314
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2022-07-05 14:13:59 +0300

    make app use .env

--- src/app.py
+++ src/app.py
@@ -3,10 +3,12 @@ import os
 from flask import Flask
 
 import AppFactory
+from dotenv import load_dotenv
+load_dotenv()
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(ROOT_DIR, 'userFiles')
-DATABASE_URL = 'postgresql://postgres:ps@localhost:5432/file_database'
+DATABASE_URL = os.getenv("DB_URL")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
