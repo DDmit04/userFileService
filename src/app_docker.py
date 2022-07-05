c93460cd0d54e1c37b41ca66e75140d85cb6a980
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2022-07-05 15:27:33 +0300

    update docker

--- src/app_docker.py
+++ src/app_docker.py
@@ -11,7 +11,11 @@ SEPARATOR = os.environ['SEPARATOR']
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['PATH_SEPARATOR'] = SEPARATOR
 app = AppFactory.setup_app(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
