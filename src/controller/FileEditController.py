5db2594df06a4e6377807449b72dfd800c98ed2b
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2022-07-06 14:34:25 +0300

    move functions from controller to facade

--- src/controller/FileEditController.py
+++ src/controller/FileEditController.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 from flask_expects_json import expects_json
 
+from src.dependency.DependencyContainer import di_container
 from src.model import FileRecord
 from src.model.requests.RequestSchemas import file_comment_update_schema, filename_update_schema, filepath_update_schema
-from src.service import FileRecordService, FileService
-from src.dependency.DependencyContainer import di_container
+from src.service import FileRecordService
 from src.utils.ControllerUtils import exception_handle
 
 file_edit_controller_blueprint = Blueprint('file_edit_controller', __name__)
@@ -24,25 +24,17 @@ def update_file_comment(file_id: int):
 @exception_handle
 @expects_json(filename_update_schema)
 def update_filename(file_id: int):
-    # TODO to facade
-    file_record_service: FileRecordService = di_container.file_record_service
-    file_service: FileService = di_container.file_service
     new_name = request.json['name']
-    file_record_to_update: FileRecord = file_record_service.get_file_record(file_id)
-    file_service.update_filename(file_record_to_update, new_name)
-    file_record_to_update = file_record_service.update_file_name(file_id, new_name)
-    return jsonify(file_record_to_update)
+    file_service_facade = di_container.file_service_facade
+    updated_record = file_service_facade.update_filename(file_id, new_name)
+    return jsonify(updated_record)
 
 
 @file_edit_controller_blueprint.route('<file_id>/path', methods=["PATCH"])
 @exception_handle
 @expects_json(filepath_update_schema)
 def update_file_path(file_id: int):
-    # TODO to facade
-    file_record_service: FileRecordService = di_container.file_record_service
-    file_service: FileService = di_container.file_service
     new_path = request.json['path']
-    file_record_to_update: FileRecord = file_record_service.get_file_record(file_id)
-    file_service.update_file_path(file_record_to_update, new_path)
-    file_record_to_update = file_record_service.update_file_path(file_id, new_path)
-    return jsonify(file_record_to_update)
+    file_service_facade = di_container.file_service_facade
+    updated_record = file_service_facade.update_file_path(file_id, new_path)
+    return jsonify(updated_record)
--- src/service/FileServiceFacade.py
+++ src/service/FileServiceFacade.py
@@ -38,3 +38,17 @@ class FileServiceFacade(TransactionRequiredService):
         file_to_delete_info: FileRecord = file_record_service.get_file_record(file_id)
         file_record_service.delete_file_record(file_id)
         file_service.delete_file(file_to_delete_info)
+
+    @transactional
+    def update_filename(self, file_id: int, new_name: str) -> FileRecord:
+        file_record_to_update: FileRecord = self._file_record_service.get_file_record(file_id)
+        self._file_service.update_filename(file_record_to_update, new_name)
+        file_record_to_update = self._file_record_service.update_file_name(file_id, new_name)
+        return file_record_to_update
+
+    @transactional
+    def update_file_path(self, file_id: int, new_path: str) -> FileRecord:
+        file_record_to_update: FileRecord = self._file_record_service.get_file_record(file_id)
+        self._file_service.update_file_path(file_record_to_update, new_path)
+        file_record_to_update = self._file_record_service.update_file_path(file_id, new_path)
+        return file_record_to_update
